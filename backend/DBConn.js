cd9b5011acf88b5b20c4189388b4cbbbf0218631
Meet6878 <1160meetsathvara@gmail.com>
2024-12-19 23:37:56 +0530

    connect db from env

--- backend/DBConn.js
+++ backend/DBConn.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const DBconnection = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://1160meetsathvara:meet6878@cluster0.tjmhndb.mongodb.net/chats`
-    );
+    await mongoose.connect(process.env.DB_URL);
     console.log("connect to database successfully");
   } catch (error) {
     console.log(error);
